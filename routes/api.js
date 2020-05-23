bfd632c1fd0da677f32579a6a8cdc9a2157a59c7
Glitch (anon-messngr-hm) <none>
2020-05-23 15:04:08 +0000

    post thread working

--- routes/api.js
+++ routes/api.js
@@ -22,39 +22,44 @@ module.exports = function (app) {
   
   
   app.route('/api/threads/:board')
+  
     .post((req, res) => {
-    const text = req.params.text;
-    const delete_password = req.params.delete_password;
+    const board = req.params.board;
+    const text = req.body.text;
+    const delete_password = req.body.delete_password;
     const callback = (data) => {
-      if (data.delete_password !== delete_password) alert("redirecting to a pre-existing thread");
-      res.redirect(`/${data.text}`);
+      //if (data.delete_password !== delete_password) res.send("redirecting to a pre-existing thread");
+      res.redirect(`/b/${board}/${data._id}`);
     }
-    updateThread.createThread(text, delete_password, callback);
+    updateThread.createThread(text, delete_password, callback, board);
     })
   
     .get((req, res) => {
+    const board = req.params.board;
     const callback = (data) => {
       //filter out delete_password and reported fields as well as only top 3 replies
     };
-    updateThread.displayThreads(callback);
+    updateThread.displayThreads(callback, board);
     })
   
     .delete((req, res) => {
+    const board = req.params.board;
     const thread_id = req.params.thread_id;
     const delete_password = req.params.delete_password;
     const callback = (data) => {
-      data.value ? res.text('success') : res.text('incorrect password');
+      data.value ? res.send('success') : res.text('incorrect password');
     };
     
-    updateThread.deleteThread(thread_id, delete_password, callback);
+    updateThread.deleteThread(thread_id, delete_password, callback, board);
     })
   
     .put((req, res) => {
+    const board = req.params.board;
     const thread_id = req.params.thread_id;
     const callback = (data) => {
-      if (data) res.text('success');
+      if (data) res.send('success');
     };
-    updateThread.reportThread = (thread_id, callback);
+    updateThread.reportThread = (thread_id, callback, board);
     });
   
     
